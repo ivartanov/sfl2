5d17784859bf82328e551ac07381423994440dc8
igor.vartanov <igor.vartanov@dsr-corporation.com>
2024-07-01 19:47:19 +0300

    MinSvc - improve make script a bit

--- examples/MinSvc/make.bat
+++ examples/MinSvc/make.bat
@@ -1,20 +1,38 @@
 @echo off
 
 set SFLDIR=..\..\include
+set DBGOPT=
+set BLDCMD=0
+set BLDTYPE=%1
 
 if "%1" == "clean" (
-  call :cleanup
-  exit
+ call :cleanup
+ exit /b
+) else if "%1" == "debug" (
+  set DBGOPT=/Zi
+  set BLDCMD=1
+) else if "%1" == "release" (
+  set BLDCMD=1
+) else if ".%1" == "." (
+  rem Debug implied
+  set DBGOPT=/Zi
+  set BLDCMD=1
+  set BLDTYPE=debug
 )
 
-call :build
+if %BLDCMD% == 1 (
+  call :build %BLDTYPE%
+  exit /b
+) else (
+  echo Syntax is wrong.
+  exit /b
+)
 
 :cleanup
-del /Q *.obj *.exe
-exit
+del /Q *.obj *.exe *.pdb *.ilk
+exit /b
 
 :build
-echo "Building ..."
-cl MinSvc.cpp /MD /EHsc /I%SFLDIR% /link /subsystem:console
-exit
-
+echo Building %1...
+cl MinSvc.cpp /MD /EHsc /I%SFLDIR% %DBGOPT% /link /subsystem:console
+exit /b
